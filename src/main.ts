9bfbde9e94445a5e88a61a6a644aa2a151198606
chenwenzhuo <Chenwz1996@163.com>
2023-06-02 18:56:30 +0800

    集成ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, {locale: zhCn,})
+//注册所有ElementPlus图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app')
