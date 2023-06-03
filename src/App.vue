ad7d904fcfb516a8d3b16bcd92bcb58cb9ef12d0
chenwenzhuo <Chenwz1996@163.com>
2023-06-03 17:20:53 +0800

    API接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted} from "vue";
-import request from "@/utils/request";
 
-onMounted(() => {
-    request({
-        url: '/user/login',
-        method: 'post',
-        data: {
-            username: 'admin',
-            password: '111111'
-        }
-    })
-})
 </script>
 
 <style scoped lang="scss">
---
+++ src/api/user/types.ts
@@ -0,0 +1,37 @@
+//登录接口携带的参数ts类型
+export interface loginForm {
+    username: string,
+    password: string,
+}
+
+//登录接口返回的数据类型
+interface dataType {
+    token: string
+}
+
+export interface loginResponseData {
+    code: number,
+    data: dataType,
+}
+
+//服务器返回用户信息相关的数据类型
+interface userInfo {
+    userId: number,
+    avatar: string,
+    username: string,
+    password: string,
+    desc: string,
+    roles: string[],
+    buttons: string[],
+    routes: string[],
+    token: string,
+}
+
+interface user {
+    checkUser: userInfo
+}
+
+export interface userResponseData {
+    code: number,
+    data: user,
+}
