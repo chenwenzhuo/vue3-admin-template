c5d7cb2b045c10eca356f5805ebbbb44d5a46478
chenwenzhuo <Chenwz1996@163.com>
2023-06-06 17:47:14 +0800

    路由切换时添加进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import {routesConfig} from './routes'
+// @ts-ignore
+import nprogress from "nprogress";
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -8,6 +11,20 @@ const router = createRouter({
     scrollBehavior() {
         return {left: 0, top: 0}
     }
+});
+
+//通过全局前置路由守卫进行路由鉴权
+//全局前置守卫
+router.beforeEach(async (to: any, from: any, next: any) => {
+    //访问任意路由前都会执行
+    nprogress.start();//开始展示进度条
+    next();
+});
+
+//全局后置守卫
+router.afterEach(async (to: any, from: any) => {
+    //访问任意路由后执行
+    nprogress.done();//隐藏进度条
 });
 
 export default router
