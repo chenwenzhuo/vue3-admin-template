e0dd13330b33054bcb2bd272d1434fcd231ce496
chenwenzhuo <Chenwz1996@163.com>
2023-06-17 18:11:13 +0800

    完成角色添加、更新功能

--- src/api/acl/role/index.ts
+++ src/api/acl/role/index.ts
@@ -1,11 +1,22 @@
 //角色管理相关接口
 import request from "@/utils/request";
-import type {RoleResponseData} from "@/api/acl/role/types";
+import type {RoleData, RoleResponseData} from "@/api/acl/role/types";
 
 enum API {
     ALL_ROLE_URL = '/admin/acl/role',//获取全部的职位
+    ADD_ROLE_URL = '/admin/acl/role/save',//新增岗位的接口地址
+    UPDATE_ROLE_URL = '/admin/acl/role/update',//更新已有的职位
 }
 
 //获取全部的职位
 export const reqAllRoles = (pageNo: number, limit: number, roleName: string) =>
     request.get<any, RoleResponseData>(`${API.ALL_ROLE_URL}/${pageNo}/${limit}/?roleName=${roleName}`);
+
+//添加职位与更新已有职位接口
+export const reqAddOrUpdateRole = (data: RoleData) => {
+    if (!!data.id) {
+        return request.put<any, any>(API.UPDATE_ROLE_URL, data);
+    } else {
+        return request.post<any, any>(API.ADD_ROLE_URL, data);
+    }
+}
--- src/views/acl/Role.vue
+++ src/views/acl/Role.vue
@@ -18,7 +18,7 @@
   <!--角色列表部分-->
     <el-card class="box-card-table">
         <template #header>
-            <el-button type="primary" icon="Plus">添加角色</el-button>
+            <el-button type="primary" icon="Plus" @click="handleAddRole">添加角色</el-button>
         </template>
 
         <el-table border :data="roleTableData.data">
@@ -30,7 +30,7 @@
             <el-table-column label="操作" width="280" align="center" fixed="right">
                 <template #default="{row}">
                     <el-button type="primary" icon="Filter" size="small">分配权限</el-button>
-                    <el-button type="primary" icon="Edit" size="small">编辑</el-button>
+                    <el-button type="primary" icon="Edit" size="small" @click="handleUpdateRole(row)">编辑</el-button>
                     <el-button type="danger" icon="Delete" size="small">删除</el-button>
                 </template>
             </el-table-column>
@@ -40,16 +40,38 @@
                        v-model:current-page="pageNo" v-model:page-size="pageSize"
                        :page-sizes="[10,20,30]" :total="total"
                        @current-change="getAllRoles" @size-change="getAllRoles"/>
+
+        <!--添加、更新角色弹窗-->
+        <el-dialog :model-value="dialogDisplayStatus!==0" :title="dialogTitle"
+                   @close="onDialogClose" :width="dialogWidth">
+            <el-form ref="addUpdateFormRef" :model="addUpdateRoleData.data"
+                     :rules="addUpdateFormRules">
+                <el-form-item prop="roleName" label="角色名称">
+                    <el-input v-model="addUpdateRoleData.data.roleName" placeholder="请输入角色名称"/>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <el-button type="primary" @click="confirmAddUpdate">确定</el-button>
+                <el-button @click="onDialogClose">取消</el-button>
+            </template>
+        </el-dialog>
     </el-card>
 </template>
 
 <script setup lang="ts">
-import {onMounted, reactive, ref} from "vue";
-import type {RoleRecords, RoleResponseData} from "@/api/acl/role/types";
-import {reqAllRoles} from "@/api/acl/role";
+import {computed, nextTick, onMounted, reactive, ref} from "vue";
+import type {FormInstance, FormRules} from "element-plus";
+
+import type {RoleData, RoleRecords, RoleResponseData} from "@/api/acl/role/types";
+import {reqAddOrUpdateRole, reqAllRoles} from "@/api/acl/role";
+import {ElMessage} from "element-plus";
 
 interface RoleTableDataType {
     data: RoleRecords
+}
+
+interface AddOrUpdateRoleDataType {
+    data: RoleData
 }
 
 let searchRoleKeyword = ref<string>('');//搜索角色的关键字
@@ -57,6 +79,33 @@ let pageNo = ref<number>(1);//分页组件当前页码
 let pageSize = ref<number>(10);//分页组件页大小
 let total = ref<number>(0);//分页组件数据总条数
 let roleTableData = reactive<RoleTableDataType>({data: []});//角色表格数据
+
+//控制添加、更新角色弹窗显示状态。0-隐藏，1-添加角色，2-更新角色
+let dialogDisplayStatus = ref<number>(0);
+let dialogTitle: string = computed(() => {
+    if (dialogDisplayStatus.value === 1)
+        return '添加角色';
+    else if (dialogDisplayStatus.value === 2)
+        return '更新角色';
+    return '';
+});
+//控制添加、更新角色弹窗宽度
+let dialogWidth = computed(() => {
+    const clientWidth = document.body.clientWidth;
+    if (clientWidth < 1200)
+        return '50%';
+    return '500px';
+});
+const addUpdateFormRef = ref<FormInstance>();//添加、更新角色表单实例引用
+let addUpdateRoleData = reactive<AddOrUpdateRoleDataType>({//添加、更新角色表单数据
+    data: {roleName: ''}
+});
+const addUpdateFormRules = reactive<FormRules>({
+    roleName: [
+        {required: true, message: '角色名称必须输入'},
+        {min: 2, message: '角色名称长度不少于2位'},
+    ]
+});
 
 //查询所有角色信息
 const getAllRoles = async () => {
@@ -81,6 +130,44 @@ const resetSearch = () => {
     searchRole();
 }
 
+//添加角色按钮点击事件
+const handleAddRole = () => {
+    dialogDisplayStatus.value = 1;
+    nextTick(() => addUpdateFormRef.value?.clearValidate());
+}
+
+//更新角色按钮点击事件
+const handleUpdateRole = (row: RoleData) => {
+    dialogDisplayStatus.value = 2;
+    nextTick(() => addUpdateFormRef.value?.clearValidate());
+    Object.assign(addUpdateRoleData.data, row);
+}
+
+//确认添加、更新角色按钮点击事件
+const confirmAddUpdate = () => {
+    addUpdateFormRef.value?.validate(async valid => {
+        if (!valid)
+            return;
+        const result: any = await reqAddOrUpdateRole(addUpdateRoleData.data);
+        if (result.code === 200) {
+            ElMessage.success(`${dialogDisplayStatus.value === 1 ? '添加' : '更新'}角色成功！`);
+            //重新查询数据
+            if (dialogDisplayStatus.value === 1)
+                pageNo.value = 1;//添加角色后，表格回到第一页
+            getAllRoles();
+            onDialogClose();//关闭弹窗
+        } else {
+            ElMessage.error(`${dialogDisplayStatus.value === 1 ? '添加' : '更新'}角色失败！${result.data}`);
+        }
+    });
+}
+
+//添加、更新角色弹窗关闭事件
+const onDialogClose = () => {
+    dialogDisplayStatus.value = 0;
+    addUpdateRoleData.data = {};
+}
+
 onMounted(() => {
     getAllRoles();
 })
@@ -102,6 +189,10 @@ onMounted(() => {
 
   .el-pagination {
     margin-top: 15px;
+  }
+
+  .add-update-dialog {
+    max-width: 500px;
   }
 }
 </style>
